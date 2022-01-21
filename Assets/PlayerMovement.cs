7f255474142aa4d942fb4af9f9bd45b9162f7277
Pietietjie <pietervdm72@gmail.com>
2022-01-21 08:00:08 +0200

    feat: added constant movement for the player

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void Update()
+    // FixedUpdate is like update but works better with phisics
+    void FixedUpdate()
     {
-        
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);        
     }
 }
