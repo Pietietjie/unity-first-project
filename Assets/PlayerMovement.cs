9e0416ee4de434d7e6c37e58c5b185aaad3e8e2d
Pietietjie <pietervdm72@gmail.com>
2022-01-21 10:10:53 +0200

    feat: added side movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
+    public float dodgeSpeed = 500f;
 
     // Start is called before the first frame update
     void Start()
@@ -13,6 +14,15 @@ public class PlayerMovement : MonoBehaviour
     // FixedUpdate is like update but works better with phisics
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 500 * Time.deltaTime);        
+        rb.AddForce(0, 0, 500 * Time.deltaTime);
+
+        if (Input.GetKey("d")) {
+            rb.AddForce(dodgeSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a")) {
+            rb.AddForce(-dodgeSpeed * Time.deltaTime, 0, 0);
+
+        }
     }
 }
